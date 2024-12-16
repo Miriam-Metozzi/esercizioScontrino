cf2e1a488c4f3635705545c5813b00c3d64ecc52
Miriam Metozzi <miriam.metozzi@gmail.com>
2024-12-16 18:03:44 +0100

    esercizio concluso davvero era ora

--- scontrino.py
+++ scontrino.py
@@ -9,5 +9,8 @@ price = input("Quanto costa? ")
 qnt = input("Quanti ne compri? ")
 
 #metodo1 - calcolare il risultato e metterlo in una variabile
-tot = int(qnt) * float(price)
-print("Per comprare " + qnt + " di " + item + " devi pagare " +str(tot) + "€")
+tot = round(int(qnt) * float(price), 2)
+print("Per comprare " + qnt + " di " + item + " devi pagare " +str(round(tot, 2)) + "€")
+
+#metodo2 - sfrutto le f-strings
+print(F"Per comprare {qnt} di {item} devi pagare {round(int(qnt) * float(price), 2)}€")
