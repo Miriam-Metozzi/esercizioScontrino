d67a0d1007b669b80957cc21c4d4a4d9796e417d
Miriam Metozzi <miriam.metozzi@gmail.com>
2024-12-17 09:44:00 +0100

    esercizio concluso con 3 metodi

--- scontrino.py
+++ scontrino.py
@@ -1,5 +1,7 @@
 #16/12/24
-# Programma che visualizza uno scontrino relativo all'acquisto di un prodotto con dati:
+
+# Programma che visualizza uno scontrino relativo
+# all'acquisto di un prodotto con dati:
 # nome del prodotto
 # quantità desiderata
 # prezzo
@@ -8,9 +10,18 @@ item = input("Che cosa vuoi comprare? ")
 price = input("Quanto costa? ")
 qnt = input("Quanti ne compri? ")
 
-#metodo1 - calcolare il risultato e metterlo in una variabile
+#metodo 1 - calcolare il risultato e metterlo in una variabile
 tot = round(int(qnt) * float(price), 2)
-print("Per comprare " + qnt + " di " + item + " devi pagare " +str(round(tot, 2)) + "€")
+# print(type(tot))
+# print(type(tot))
 
-#metodo2 - sfrutto le f-strings
+print("Per comprare " + qnt + " di " + item + " devi pagare " +str(tot) + "€")
+
+#metodo 2 - sfrutto le f-strings
 print(F"Per comprare {qnt} di {item} devi pagare {round(int(qnt) * float(price), 2)}€")
+
+# metodo 3 - fare le conversioni all'inizio
+item1 = input("Che cosa vuoi comprare? ")
+price1 = float(input("Quanto costa? "))
+qnt1 = int(input("Quanti ne compri? "))
+print(F"Per comprare {qnt} di {item} devi pagare {round(qnt1 * price1, 2)}€")
